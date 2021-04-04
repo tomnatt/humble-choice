d946c96fb7d0b5ab38200b50328ad24535c9ccac
tomnatt <github@tomnatt.com>
2021-04-04 22:45:28 +0100

    Look up steam ids and populate game objects before output
    Also update output files.

---
+++ lib/steam_ids.rb
@@ -0,0 +1,24 @@
+require 'json'
+require 'net/http'
+require 'uri'
+require_relative './game'
+
+class SteamIds
+  def initialize
+    steam_ids_url = 'http://api.steampowered.com/ISteamApps/GetAppList/v0002/?format=json'
+    steam_ids_raw_json = Net::HTTP.get(URI.parse(steam_ids_url))
+
+    @steam_ids = JSON.parse(steam_ids_raw_json)
+  end
+
+  def populate_all_games(output)
+    output.each_value do |games_list|
+      games_list.each do |game|
+        g = @steam_ids['applist']['apps'].filter { |a| a['name'].downcase == game.name.downcase }
+        game.steam_id = g.first['appid'] unless g.empty?
+      end
+    end
+
+    output
+  end
+end
