dfd3eb7c097c2d6d989f44aa9f58096227aba6f7
tomnatt <github@tomnatt.com>
2021-05-23 16:03:33 +0100

    When there is a name clash, ensure the chosen steam id is deterministic
    Previously was taking whichever came out last. Instead, take the higher
    value as this should be the later entry and will only change if the
    source data changes.

--- lib/steam_ids.rb
+++ lib/steam_ids.rb
@@ -14,11 +14,17 @@ class SteamIds
   def populate_all_games(output)
     output.each_value do |games_list|
       games_list.each do |game|
-        g = @steam_ids['applist']['apps'].filter { |a| a['name'].strip.downcase == game.name.strip.downcase }
-        game.steam_id = g.last['appid'] unless g.empty?
+        # Match game and if it matches more than once, take the highest value - should be latest entry
+        matches = match_game(game)
+        game.steam_id = matches.max_by { |g| g['appid'] }['appid'] unless matches.empty?
       end
     end
 
     output
+  end
+
+  # Match game name against Steam ID list
+  def match_game(game)
+    @steam_ids['applist']['apps'].filter { |a| a['name'].strip.downcase == game.name.strip.downcase }
   end
 end
