8402b9031848e42f200704803481820f79066d10
tomnatt <github@tomnatt.com>
2025-01-15 14:52:14 +0000

    Update Google code to simplify
    Formerly was creating a Hash then sorting then removing Hash and sorting
    again. Now goes straight to Array and sorts once after combining.

--- lib/google_data.rb
+++ lib/google_data.rb
@@ -12,29 +12,25 @@ class GoogleData
     session = GoogleDrive::Session.from_service_account_key(ENV.fetch('HUMBLE_CHOICE_SERVICE_ACCOUNT_KEY'))
     @spreadsheet = session.spreadsheet_by_key('1VZHuYi0OB6kc9Ma31RG57S7GqX2ND3Gk3FFfgDkToIk')
 
-    monthly = read_data(@spreadsheet.worksheets.first, 'monthly').sort.to_h
-    choice = read_data(@spreadsheet.worksheets.last, 'choice').sort.to_h
-    # Merge Monthly and Choice lists, take just the games and flatten into a single array
-    @games_list = (monthly.values + choice.values).flatten
+    monthly = read_data(@spreadsheet.worksheets.first, 'monthly')
+    choice = read_data(@spreadsheet.worksheets.last, 'choice')
+    # Merge Monthly and Choice lists, flatten into a single array and sort by year / month / name
+    @games_list = (monthly + choice).flatten.sort_by { |g| [g.year, Date::MONTHNAMES.index(g.month), g.name] }
   end
 
   def read_data(worksheet, source)
     # Spreadsheet in format [ Month, Game ]
     # Skip first row (contains headers)
-    game_hash = {}
+    games = []
     worksheet.rows.drop(1).each do |row|
       date = Date.parse(row[0])
       year = date.year
       month = date.strftime('%B')
 
-      games = split_game_list(row[1])
-      game_hash = create_game_objects(game_hash, games, month, year, source)
+      game_names = split_game_list(row[1])
+      games << create_game_objects(game_names, month, year, source)
     end
-    reverse_years(game_hash)
-  end
-
-  def reverse_years(game_hash)
-    game_hash.each_key { |year| game_hash[year].reverse! }
+    games
   end
 
   def split_game_list(list)
@@ -61,11 +57,11 @@ class GoogleData
     games
   end
 
-  def create_game_objects(game_hash, games, month, year, scheme)
-    game_hash[year] = [] unless game_hash[year]
-    games.each do |game|
-      game_hash[year] << Game.new(game, month, year, scheme) unless game.empty?
+  def create_game_objects(game_names, month, year, scheme)
+    games = []
+    game_names.each do |game|
+      games << Game.new(game, month, year, scheme) unless game.empty?
     end
-    game_hash
+    games
   end
 end
